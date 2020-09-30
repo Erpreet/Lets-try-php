12b4a246cd3640048ca1978eee6fc473cd37c9b9
Erpreet <preetkaurchd15@gmail.com>
2020-09-30 10:12:23 -0600

    multi-line comment added

--- hello-world.php
+++ hello-world.php
@@ -3,3 +3,11 @@
 //"Echo" and "prints" are functions we can
 // use to output strings as a response
 echo "Hello, World!";
+
+# This a also a single line comment
+
+/* This is a multi-line comment
+multi
+line */
+
+
